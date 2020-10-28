6ee3fb14654c6f5685be43f13a7e245a2175b58f
Vy-Shane Xie <s@vyshane.com>
2020-10-28 18:47:23 +0800

    Export addExponentialDelay via API index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) 2020 WiseTime. All rights reserved.
 
 import { from } from "./client"
-import { RetryPolicy, retry, responseNotOk, never } from "./retry"
+import { RetryPolicy, retry, responseNotOk, never, addExponentialDelay } from "./retry"
 
 // The grpc-web-rx public API.
 export type { RetryPolicy }
-export { from, retry, responseNotOk, never }
+export { from, retry, responseNotOk, never, addExponentialDelay }
