917b0289fa9d0baa5d624a3a4dbd3f15491776ef
Marina Igumnova <marina.igumnova@staff.wisetime.io>
2020-11-02 14:54:17 +0300

    WT-11399 preparation for bamboo builds, github mirror and npm publish

---
+++ mirror.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o errexit ; set -o errtrace ; set -o pipefail
+
+git fetch --tags
+git checkout master
+mkdir -p ~/.ssh
+# Ensure we can talk to GitHub
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+mkdir -p /tmp/.ssh
+echo "${GITHUB_SSH_KEY_B64}" | base64 -d > /tmp/.ssh/github.key
+chmod 600 /tmp/.ssh/github.key
+# Push to GitHub mirror
+git remote add github git@github.com:wisetime-io/grpc-web-rx.git
+GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github master --tags
+rm -rf /tmp/.ssh
+echo "Push to GitHub mirror complete"
