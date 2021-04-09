2cc0711cdbe52f9efb4a789b00f39ecd408e624d
Yehor Lashkul <yegor.lashkul@gmail.com>
2021-04-09 15:31:01 +0300

    Avoid cascade retry on multiple server/proxy errors

--- src/test/integration/client/testUtil.ts
+++ src/test/integration/client/testUtil.ts
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 WiseTime. All rights reserved.
 
+import { ServerStreamingRpc } from "../../../client"
+import { Observable } from "rxjs"
+import Grpc from "grpc-web"
+
 /**
  * Add support for running grpc-web integration tests in a node server environment.
  */
@@ -11,6 +15,31 @@ export const setupIntegrationTests = (): void => {
     console.log("Running from browser...")
   }
 }
+
+/**
+ * Wrap an rpc call with observable that sends multiple errors on single rpc error for multi error simulation
+ */
+export const rpcWithMultipleErrors = <T>(rpc: ServerStreamingRpc): Observable<T> => (
+  new Observable<T>(observer => {
+    const call = rpc()
+    call.on("data", data =>
+      data !== undefined
+        ? observer.next(data as T)
+        : observer.error(new Error("Response payload is undefined"))
+    )
+    call.on("error", error => {
+      // simulate server/proxy to send multiple errors
+      observer.error(error)
+      observer.error(error)
+      observer.error(error)
+    })
+    call.on("status", status => {
+      if (status.code == Grpc.StatusCode.OK) {
+        observer.complete()
+      }
+    })
+  })
+)
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const testNoOp = (): void => {}
