8d5ffe3135a529a6eb60a653c38960cba472df0f
sumitpatel83 <supatel@anaqua.com>
2025-10-16 12:16:01 +0000

    Mirror Repo

---
+++ src/client.ts
@@ -0,0 +1,60 @@
+// Copyright (c) 2020 WiseTime. All rights reserved.
+
+import * as Grpc from "grpc-web"
+import { Observable, Subscriber } from "rxjs"
+
+export type UnaryRpc<T> = () => Promise<T>
+export type ServerStreamingRpc = () => Grpc.ClientReadableStream<unknown>
+
+const isServerStreaming = <T>(
+  response: ReturnType<UnaryRpc<T>> | ReturnType<ServerStreamingRpc>
+): response is ReturnType<ServerStreamingRpc> =>
+    "on" in response
+
+const fromServerStreaming = <T>(
+  call: Grpc.ClientReadableStream<unknown>,
+  observer: Subscriber<T>
+): void => {
+  call.on("data", data =>
+    data !== undefined
+      ? observer.next(data as T)
+      : observer.error(new Error("Response payload is undefined"))
+  )
+  call.on("error", error => observer.error(error))
+  call.on("status", status => {
+    if (status.code == Grpc.StatusCode.OK) {
+      observer.complete()
+    }
+  })
+}
+
+const fromUnary = <T>(
+  call: Promise<T>,
+  observer: Subscriber<T>
+): Promise<void> =>
+    call
+      .then(value => {
+        observer.next(value)
+        observer.complete()
+      })
+      .catch((err: unknown) => observer.error(err))
+
+/**
+ * Create an {@link Observable} from a gRPC call.
+ *
+ * Currently only supports unary and server streaming RPCs because {@link gRPC-web|https://github.com/grpc/grpc-web}
+ * only supports those for now.
+ *
+ * @param rpc - gRPC-web call which can either be unary or server-streaming
+ */
+export const from = <T>(
+  rpc: UnaryRpc<T> | ServerStreamingRpc
+): Observable<T> =>
+    new Observable<T>(observer => {
+      const call = rpc()
+      if (isServerStreaming<T>(call)) {
+        fromServerStreaming(call, observer)
+      } else {
+        fromUnary(call, observer)
+      }
+    })
---
+++ src/test/integration/server/server.ts
@@ -0,0 +1,35 @@
+// Copyright (c) 2020 WiseTime. All rights reserved.
+
+import * as grpc from "@grpc/grpc-js"
+import { UnaryScenarios } from "./UnaryScenarios"
+import { ServerStreamingScenarios } from "./ServerStreamingScenarios"
+import { RetryScenarios } from "./RetryScenarios"
+import { UnaryScenariosService, ServerStreamingScenariosService, RetryScenariosService } from "../../../generated/server/test_scenarios_grpc_pb"
+
+const startServer = (): void => {
+  const server = new grpc.Server()
+
+  server.addService(UnaryScenariosService, UnaryScenarios)
+  server.addService(ServerStreamingScenariosService, ServerStreamingScenarios)
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  server.addService(RetryScenariosService, new RetryScenarios())
+
+  server.bindAsync("0.0.0.0:9090", grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if (err) {
+      throw err
+    }
+    console.log(`Starting gRPC server, listening on: 127.0.0.1:${port}`)
+    server.start()
+  })
+}
+
+startServer()
+
+process.on("uncaughtException", (err) => {
+  console.log(`process on uncaughtException error: ${err}`)
+})
+
+process.on("unhandledRejection", (err) => {
+  console.log(`process on unhandledRejection error: ${err}`)
+})
